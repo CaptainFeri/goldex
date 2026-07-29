c19fb6fa9c5bbd443e2833664161f90905c3ffaf
ferriloper <captainferigit@gmail.com>
2026-07-29 12:46:33 +0330

    update pricing engine and android app

--- goldex-sms-forwarder/app/src/main/java/com/goldex/smsforwarder/ui/ProviderAuthController.kt
+++ goldex-sms-forwarder/app/src/main/java/com/goldex/smsforwarder/ui/ProviderAuthController.kt
@@ -1,6 +1,7 @@
 package com.goldex.smsforwarder.ui
 
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.util.Log
 import android.webkit.JavascriptInterface
